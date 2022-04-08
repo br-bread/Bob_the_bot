6d7210d04216f5b8aed95475812316a6b2036539
ksu_sha <ktsykanova@yandex.ru>
2022-04-08 23:33:28 +0300

    bochka meda

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ TOKEN = '5235508319:AAH_BNimCWuKBi1K2h71zey92tq1RMMmreg'
 
 def main():
     updater = Updater(TOKEN)
-    dp =  updater.dispatcher
+    dp = updater.dispatcher
     updater.start_polling()
     updater.idle()
 
